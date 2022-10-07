974201bfe304a7051da7d11b9f729c69e503e262
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-10-07 15:40:20 +0100

    :wrench: Add LB Target Group

--- main.tf
+++ main.tf
@@ -104,3 +104,20 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
