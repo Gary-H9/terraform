30155c91308c57407aadfd4e3caa23f98e82d10e
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-10-07 15:50:47 +0100

    :wrench: Add target group to ASG

--- main.tf
+++ main.tf
@@ -51,6 +51,9 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
 
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+  
   min_size = 2
   max_size = 10
 
