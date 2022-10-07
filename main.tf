95c174520e79fdd4eacc10a6c916078c5456a50c
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-10-07 15:52:05 +0100

    :wrench: Add listener rule

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_autoscaling_group" "example" {
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
-  
+
   min_size = 2
   max_size = 10
 
@@ -122,5 +122,21 @@ resource "aws_lb_target_group" "asg" {
     timeout             = 3
     healthy_threshold   = 2
     unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
