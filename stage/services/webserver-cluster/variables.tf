dac9744abed61dc3539618616fe715ab47a7f70d
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-10-11 22:16:51 +0100

    :recycle: Refactor preceding work into folders

---
+++ stage/services/webserver-cluster/variables.tf
@@ -0,0 +1,15 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  type        = string
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  type        = string
+}
