913174125aca37eddcdbf2703da5295472a9c8af
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-10-12 15:51:45 +0100

    :wrench: Update to remove semi-redundant code

--- stage/services/webserver-cluster/variables.tf
+++ stage/services/webserver-cluster/variables.tf
@@ -9,7 +9,7 @@ variable "db_remote_state_bucket" {
   type        = string
 }
 
-variable "db_remote_state_key" {
-  description = "The name of the key in the S3 bucket used for the database's remote state storage"
-  type        = string
-}
+# variable "db_remote_state_key" {
+#   description = "The name of the key in the S3 bucket used for the database's remote state storage"
+#   type        = string
+# }
