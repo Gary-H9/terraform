ca3611fb7d2347ed7c8e53d86ed2d48a865f150a
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-10-12 14:23:43 +0100

    :wrench: Add terraform backend configuration

--- global/s3/main.tf
+++ global/s3/main.tf
@@ -1,6 +1,16 @@
 terraform {
   required_version = ">= 1.0.0, < 2.0.0"
 
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "gary-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
