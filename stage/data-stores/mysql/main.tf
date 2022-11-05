4c848a9c45c9f301685d5341fc5196f157c33993
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-11-05 15:40:30 +0000

    :recycle: Add s3 backend to main.tf
    gs

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -1,6 +1,5 @@
-provider "aws" {
-  region = "us-east-2"
-}
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
 
   backend "s3" {
     bucket         = "gary-up-and-running-state"
@@ -10,6 +9,17 @@ provider "aws" {
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
 
 resource "aws_db_instance" "example" {
   identifier_prefix   = "terraform-up-and-running"
