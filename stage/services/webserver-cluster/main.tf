b40c2bfa69e9ba3ff5749459bf8782510fee06f6
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-11-05 15:52:26 +0000

    :wrench: Update webserver terraform_remote_state

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -157,12 +157,12 @@ data "aws_subnets" "default" {
   }
 }
 
-# data "terraform_remote_state" "db" {
-#   backend = "s3"
+data "terraform_remote_state" "db" {
+  backend = "s3"
 
-#   config = {
-#     bucket = var.db_remote_state_bucket
-#     key    = var.db_remote_state_key
-#     region = "us-east-2"
-#   }
-# }
+  config = {
+    bucket = "gary-up-and-running-state"
+    key    = "stage/data-stores/mysql/terraform.tfgsstate"
+    region = "us-east-2"
+  }
+}
