e04a9287fd2ae63b194a393a46c6e3eaef815373
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-03-27 16:05:41 +0100

    :sparkles: Add mysql DB

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "gary-up-and-running-state"
+    key            = "stage/data-stores/mysql/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks-2"
+    encrypt        = true
+  }
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  username = var.db_username
+  password = var.db_password
+}
+
+output "address" {
+  value       = aws_db_instance.example.address
+  description = "Connect to the database at this endpoint"
+}
+
+output "port" {
+  value       = aws_db_instance.example.port
+  description = "The port the database is listening on"
+}
