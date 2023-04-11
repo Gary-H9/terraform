f286b0f3594e82b40d0612f6dab8c67ef4ad5e02
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-04-11 13:55:57 +0100

    :wrench: Add scaling in/out with business hours

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -3,3 +3,7 @@ output "alb_dns_name" {
   description = "The domain name of the load balancer"
 }
 
+output "asg_name" {
+  value       = aws_autoscaling_group.example.name
+  description = "The name of the Auto Scaling Group"
+}
