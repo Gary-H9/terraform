1af9dcc5f73c9bdd1f6fbd18a51f7a4b64fb6838
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-04-11 16:16:34 +0100

    :wrench: Add S3 backend configuration

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -7,6 +7,17 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+    # manually, uncomment and fill in the config below.
+
+    # bucket         = "<YOUR S3 BUCKET>"
+    # key            = "<SOME PATH>/terraform.tfstate"
+    # region         = "us-east-2"
+    # dynamodb_table = "<YOUR DYNAMODB TABLE>"
+    # encrypt        = true
+  }
 }
 
 provider "aws" {
