48481d3fbb288b1c669a813bc713811374e3fd90
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-04-11 13:43:27 +0100

    :wrench: Alter cluster size depending on environment

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -8,4 +8,8 @@ module "webserver_cluster" {
   cluster_name           = "webservers-prod"
   db_remote_state_bucket = "gary-up-and-running-state"
   db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "m4.large"
+  min_size      = 2
+  max_size      = 10
 }
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -8,4 +8,8 @@ module "webserver_cluster" {
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "gary-up-and-running-state"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
 }
