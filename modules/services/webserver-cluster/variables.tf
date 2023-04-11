e2c86e1c762c6e0c8b89767944e57d6b2d1ac8ce
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-04-11 15:10:32 +0100

    :coffin: Remove duplicate variables

--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -36,16 +36,6 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "db_remote_state_bucket" {
-  description = "The name of the S3 bucket for the database's remote state"
-  type        = string
-}
-
-variable "db_remote_state_key" {
-  description = "The path for the database's remote state in S3"
-  type        = string
-}
-
 variable "instance_type" {
   description = "The type of EC2 Instances to run (e.g. t2.micro)"
   type        = string
