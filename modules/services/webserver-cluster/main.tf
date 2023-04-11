147b5ee54966fc855b4c483c68b0cef900f82556
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-04-11 13:43:27 +0100

    :wrench: Alter cluster size depending on environment

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 resource "aws_launch_configuration" "example" {
   image_id               = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
   # Render the User Data script as a template
@@ -45,12 +45,12 @@ resource "aws_autoscaling_group" "example" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size = 2
-  max_size = 10
+  min_size = var.min_size
+  max_size = var.max_size
 
   tag {
     key                 = "Name"
-    value               = "${var.cluster_name}-example"
+    value               = var.cluster_name
     propagate_at_launch = true
   }
 }
