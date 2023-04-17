4d41ebe3559d8d8610ca29830c543f5aad8f02da
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-04-17 15:37:48 +0100

    :recycle: Add multi-region RDS module

--- modules/data-stores/mysql/main.tf
+++ modules/data-stores/mysql/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
 terraform {
   required_version = ">= 1.0.0, < 2.0.0"
 
--- prod/data-stores/mysql/main.tf
+++ prod/data-stores/mysql/main.tf
@@ -1,40 +1,35 @@
-terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-
-  backend "s3" {
-    bucket         = "gary-up-and-running-state"
-    key            = "prod/data-stores/mysql/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "terraform-up-and-running-locks-2"
-    encrypt        = true
-  }
+provider "aws" {
+  region = "us-east-2"
+  alias  = "primary"
 }
 
 provider "aws" {
-  region = "us-east-2"
-
-  default_tags {
-    tags = {
-      Owner = "team-foo"
-      ManagedBy = "Terraform"
-    }
-  }
+  region = "us-west-1"
+  alias  = "replica"
 }
 
-resource "aws_db_instance" "example" {
-  identifier_prefix   = "terraform-up-and-running"
-  engine              = "mysql"
-  allocated_storage   = 10
-  instance_class      = "db.t2.micro"
-  db_name             = var.db_name
-  username            = var.db_username
-  password            = var.db_password
-  skip_final_snapshot = true
+module "mysql_primary" {
+  source = "../../../modules/data-stores/mysql"
+
+  providers = {
+    aws = aws.primary
+  }
+
+  db_name     = "prod_db"
+  db_username = var.db_username
+  db_password = var.db_password
+
+  # Must be enabled to support replication
+  backup_retention_period = 1
+}
+
+module "mysql_replica" {
+  source = "../../../modules/data-stores/mysql"
+
+  providers = {
+    aws = aws.replica
+  }
+
+  # Make this a replica of the primary
+  replicate_source_db = module.mysql_primary.arn
 }
